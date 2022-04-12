b201b9a00a81ee5b9d7d4f51bd8447444b255c76
Laura Walters <laura.walters1098@yahoo.com>
2022-04-12 21:00:39 +0100

    ammending env variable names

--- s3.js
+++ s3.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const fs = require("fs");
 const S3 = require("aws-sdk/clients/s3");
 
-const bucketName = process.env.AWS_BUCKET_NAME;
+const bucketName = process.env.S3_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3 = new S3({
   region,
